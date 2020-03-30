a35d1eecedee8a128003f3cdfaa59a3209d93b37
feelingliu@aliyun.com <feelingliu@aliyun.com>
2020-03-30 09:51:15 +0800

    change leetcode page

--- src/pages/leetcode.vue
+++ src/pages/leetcode.vue
@@ -1,13 +1,28 @@
 <template>
-    <div></div>
+  <div>
+    <nav-header></nav-header>
+    <pic-plug></pic-plug>
+    <articles></articles>
+    <nav-footer></nav-footer>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "leetcode"
-    }
+import NavHeader from "../components/NavHeader";
+import NavFooter from "../components/NavFooter";
+import PicPlug from "../components/PicPlug";
+import Articles from "../components/Articles";
+
+export default {
+  name: "leetcode",
+  components: {
+    PicPlug,
+    NavHeader,
+    NavFooter,
+    Articles
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
