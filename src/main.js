54a44f0611af2d862e246bab5bb8f6745e886e24
liuhanwen <liuhanwen@hualala.com>
2020-09-23 19:59:20 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,24 +4,12 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueCookie from 'vue-cookie'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
 
 Vue.use(VueAxios, axios);
 Vue.use(VueCookie);
-Vue.use(ElementUI);
 Vue.use(ViewUI);
-
-// 设置title
-// routers.beforeEach((to, from, next) => {
-//   if (to.meta.title) {
-//     document.title = to.meta.title
-//   }
-//   next()
-// })
-
 
 axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000;
